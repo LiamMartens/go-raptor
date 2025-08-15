afc7516e31a60a89ac9004404e3e92f3f381adba
Liam Martens <hi@liammartens.com>
2025-08-15 07:39:41 -0400

    feat: add additional test cases

--- raptor_test.go
+++ raptor_test.go
@@ -22,6 +22,10 @@ func TestForwardRaptor(t *testing.T) {
 
 	feed.Parse("./gtfs_subway.zip")
 
+	for _, service := range feed.Services {
+		fmt.Printf("%#v\n", service.GetFirstActiveDate().Day())
+	}
+
 	all_stops_by_id := map[string]*gtfs.Stop{}
 	parent_child_stations_by_id := map[string][]string{}
 
@@ -97,7 +101,109 @@ func TestForwardRaptor(t *testing.T) {
 			Mode:             RaptorModeDepartAt,
 			DateOfService:    "20250814",
 			TimeInSeconds:    9 * 3600,
-			MaximumTransfers: 1,
+			MaximumTransfers: 4,
+		},
+	)
+
+	fmt.Printf("found %d journeys\n", len(journeys))
+	sort.Slice(journeys, func(i, j int) bool {
+		/* return true if I < J */
+		return journeys[i].ArrivalTimeInSeconds < journeys[j].ArrivalTimeInSeconds
+	})
+
+	fmt.Printf("the first available journey departs from %s at %s arrives at %s by %s\n", journeys[0].FromUniqueStopID, FormatSecondsSinceMidnight(journeys[0].DepartureTimeInSeconds), journeys[0].ToUniqueStopID, FormatSecondsSinceMidnight(journeys[0].ArrivalTimeInSeconds))
+	for i, leg := range journeys[0].Legs {
+		if leg.ViaTrip != nil {
+			fmt.Printf("the %dnth leg takes the following trip %s at %s from stop %s to stop %s\n", i, leg.ViaTrip.UniqueTripID, FormatSecondsSinceMidnight(leg.DepartureTimeInSecondsFromUniqueStopID), leg.FromUniqueStopID, leg.ToUniqueStopID)
+		} else {
+			fmt.Printf("the %dnth transfers from %s to %s by walking\n", i, leg.FromUniqueStopID, leg.ToUniqueStopID)
+		}
+	}
+}
+
+func TestForwardRaptorLIRR(t *testing.T) {
+	feed := gtfsparser.NewFeed()
+
+	feed.Parse("./gtfslirr.zip")
+
+	for _, service := range feed.Services {
+		fmt.Printf("%#v\n", service.RawDaymap() == 0)
+	}
+
+	all_stops_by_id := map[string]*gtfs.Stop{}
+	parent_child_stations_by_id := map[string][]string{}
+
+	from_stops := []GtfsStopStruct[string]{}
+	to_stops := []GtfsStopStruct[string]{}
+	transfers := []GtfsTransferStruct[string]{}
+	stop_times := []GtfsStopTimeStruct[string]{}
+
+	for _, stop := range feed.Stops {
+		all_stops_by_id[stop.Id] = stop
+		if stop.Parent_station != nil {
+			if _, has_list := parent_child_stations_by_id[stop.Parent_station.Id]; !has_list {
+				parent_child_stations_by_id[stop.Parent_station.Id] = []string{}
+			}
+			parent_child_stations_by_id[stop.Parent_station.Id] = append(parent_child_stations_by_id[stop.Parent_station.Id], stop.Id)
+		}
+
+		/* from Nostrand Avenue */
+		if strings.HasPrefix(stop.Id, "148") {
+			from_stops = append(from_stops, GtfsStopStruct[string]{UniqueID: stop.Id})
+		}
+		/* valley stream */
+		if strings.HasPrefix(stop.Id, "211") {
+			to_stops = append(to_stops, GtfsStopStruct[string]{UniqueID: stop.Id})
+		}
+	}
+	for from_to, transfer := range feed.Transfers {
+		/*check if this is a parent station instead of an actual station */
+		from_child_stations, from_has_child_stations := parent_child_stations_by_id[from_to.From_stop.Id]
+		to_child_stations, to_has_child_stations := parent_child_stations_by_id[from_to.To_stop.Id]
+
+		transfers_possible_from := from_child_stations
+		transfers_possible_to := to_child_stations
+		if !from_has_child_stations {
+			transfers_possible_from = []string{from_to.From_stop.Id}
+		}
+		if !to_has_child_stations {
+			transfers_possible_to = []string{from_to.To_stop.Id}
+		}
+
+		for _, from_stop := range transfers_possible_from {
+			for _, to_stop := range transfers_possible_to {
+				if from_stop != to_stop {
+					transfers = append(transfers, GtfsTransferStruct[string]{
+						FromUniqueStopID:             from_stop,
+						ToUniqueStopID:               to_stop,
+						MinimumTransferTimeInSeconds: transfer.Min_transfer_time,
+					})
+				}
+			}
+		}
+	}
+	for _, trip := range feed.Trips {
+		for _, stop_time := range trip.StopTimes {
+			stop_times = append(stop_times, GtfsStopTimeStruct[string]{
+				UniqueStopID:           stop_time.Stop().Id,
+				UniqueTripID:           trip.Id,
+				ArrivalTimeInSeconds:   stop_time.Arrival_time().SecondsSinceMidnight(),
+				DepartureTimeInSeconds: stop_time.Departure_time().SecondsSinceMidnight(),
+				StopSequence:           stop_time.Sequence(),
+			})
+		}
+	}
+
+	journeys := SimpleRaptor(
+		SimpleRaptorInput[string, GtfsStopStruct[string], GtfsTransferStruct[string], GtfsStopTimeStruct[string]]{
+			FromStops:        from_stops,
+			ToStops:          to_stops,
+			Transfers:        transfers,
+			StopTimes:        stop_times,
+			Mode:             RaptorModeDepartAt,
+			DateOfService:    "20250814",
+			TimeInSeconds:    9 * 3600,
+			MaximumTransfers: 4,
 		},
 	)
 
